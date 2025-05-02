6d9487cfec88f0d2b2dd0792b0b2eda65df68e4b
Alan Panayotov <alanpanayotov@gmail.com>
2025-05-02 22:23:59 +0100

    feat: fade out file dropper

--- app/src/routes/wad/+page.svelte
+++ app/src/routes/wad/+page.svelte
@@ -2,17 +2,22 @@
 	import FileDrop from '$lib/components/FileDrop.svelte';
 	import { Button } from '$lib/components/ui/button';
 	import { Upload } from '@lucide/svelte';
+	import { fade } from 'svelte/transition';
 
 	let file: File | null = $state(null);
 </script>
 
-<FileDrop
-	class="m-5 flex-grow"
-	onFiles={(files) => {
-		file = files.item(0);
-	}}
->
-	<h2>No file open.</h2>
-	<p class="text-sm text-muted-foreground">Drag and drop a file or</p>
-	<Button>Upload<Upload /></Button>
-</FileDrop>
+{#if file === null}
+	<p class="flex flex-grow" transition:fade={{ duration: 100 }}>
+		<FileDrop
+			class="m-5 flex-grow"
+			onFiles={(files) => {
+				file = files.item(0);
+			}}
+		>
+			<h2>No file open.</h2>
+			<p class="text-sm text-muted-foreground">Drag and drop a file or</p>
+			<Button>Upload<Upload /></Button>
+		</FileDrop>
+	</p>
+{:else}{/if}
