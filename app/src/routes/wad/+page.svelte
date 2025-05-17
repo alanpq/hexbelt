e81bd991aae10e8bcf10330500658ccc0e447492
Alan Panayotov <alanpanayotov@gmail.com>
2025-05-17 11:13:06 +0100

    feat: inspector panel

--- app/src/routes/wad/+page.svelte
+++ app/src/routes/wad/+page.svelte
@@ -23,6 +23,7 @@
 	import * as Sidebar from '$lib/components/ui/sidebar';
 	import * as ContextMenu from '$lib/components/ui/context-menu';
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb';
+	import * as Resizable from '$lib/components/ui/resizable';
 	import TooltipContent from '$lib/components/ui/tooltip/tooltip-content.svelte';
 
 	import * as context from '$lib/context';
@@ -36,10 +37,14 @@
 	import DropOverlay from '$lib/components/DropOverlay.svelte';
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
+	import { browser } from '$app/environment';
+	import { cn } from '$lib/utils';
 
 	let ctx = context.wad.get();
 	let bin_ctx = context.bin.get();
 	let opening = $state(false);
+
+	let browserOpen = $derived(ctx.wad || opening);
 
 	const file_icons: Record<any, Component> = {
 		anm: SendToBack,
@@ -103,145 +108,194 @@
 			.map((i) => ctx.wad?.get(i))
 			.filter((c) => !!c);
 	});
+
+	let defaultLayout = (browser ? JSON.parse(localStorage.getItem('layout') || '0') : null) || [
+		265, 440
+	];
+	let inspectorOpen = $state(false);
+	let isCollapsed = $state(browser ? !!localStorage.getItem('wad_inspector') : true);
+	const onLayoutChange = (sizes: number[]) => {
+		if (!browser) return;
+		localStorage.setItem('layout', JSON.stringify(sizes));
+	};
+	const onCollapse = () => {
+		isCollapsed = true;
+		if (browser) localStorage.setItem('wad_inspector', '1');
+	};
+	const onExpand = () => {
+		isCollapsed = false;
+		if (browser) localStorage.removeItem('wad_inspector');
+	};
+	const onResize = (size: number, prevSize?: number) => {
+		console.log({ size, prevSize });
+		if (size < 5) inspectorOpen = false;
+	};
 </script>
 
-<main class="flex w-full flex-col p-5">
-	<header class="flex flex-row items-center gap-4">
-		<Sidebar.Trigger />
-		{#if ctx.wad}
-			<Breadcrumb.Root>
-				<Breadcrumb.List>
-					<Breadcrumb.Item>
-						<Breadcrumb.Link
-							class="cursor-pointer"
-							onclick={() => {
-								ctx.path = [];
-							}}>/</Breadcrumb.Link
-						>
-					</Breadcrumb.Item>
-					{#each ctx.path as path, i}
-						<Breadcrumb.Separator />
-						<Breadcrumb.Item>
-							<Breadcrumb.Link
-								class="cursor-pointer"
-								onclick={() => {
-									ctx.path.splice(i + 1);
-								}}>{ctx.wad.get(path)?.name}</Breadcrumb.Link
-							>
-						</Breadcrumb.Item>
-					{/each}
-				</Breadcrumb.List>
-			</Breadcrumb.Root>
-		{/if}
-	</header>
-
-	{#if !ctx.wad && !opening}
-		<div
-			class="flex flex-grow"
-			in:fade={{ delay: 100, duration: 100 }}
-			out:fade={{ duration: 100 }}
+<main class="w-full p-5">
+	<DropOverlay {onFiles} disabled={!browserOpen} innerClass="flex w-full flex-col">
+		<Resizable.PaneGroup
+			direction="horizontal"
+			{onLayoutChange}
+			class="h-full w-full items-stretch"
 		>
-			<DropZone class="m-5 flex-grow" {onFiles}>
-				<h2>No file open.</h2>
-				<p class="text-sm text-muted-foreground">Drag and drop a file or</p>
-				<Button>Upload<Upload /></Button>
-			</DropZone>
-		</div>
-	{:else}
-		<DropOverlay {onFiles}>
-			<section class="mt-5 flex-grow">
-				<ul
-					in:fade={{ delay: 100, duration: 100 }}
-					out:fade={{ duration: 100 }}
-					class="grid grid-cols-[min-content,minmax(0,1fr)] items-center gap-x-2"
-				>
-					<li class="contents">
-						<TableEntry
-							directory
-							disabled={ctx.path.length == 0}
-							onclick={() => {
-								ctx.path.splice(-1);
-							}}
-							class="cursor-pointer"
-						>
-							<Undo2 class="size-4" />
-							...
-						</TableEntry>
-					</li>
-					{#each view as item}
-						{@const is_dir = item.is_dir()}
-						{@const ext = item.name.split('.').at(-1)}
-						{@const Icon = (ext !== undefined && file_icons[ext]) || (is_dir ? Folder : FileIcon)}
-						<li class="contents">
-							<ContextMenu.Root
-								onOpenChange={(open) => {
-									if (!open) return;
-									ctx.selected = item.id;
-								}}
-							>
-								<ContextMenu.Trigger>
-									{#snippet child({ props })}
-										<TableEntry
-											{...props}
-											selected={item.id === ctx.selected}
-											directory={is_dir}
-											onclick={() => {
-												ctx.selected = item.id;
-											}}
-											ondblclick={() => {
-												if (is_dir) {
-													ctx.path.push(item.id);
-												}
-											}}
-										>
-											<Icon class="size-4" />
-											<span class="truncate"> {item.name}</span>
-										</TableEntry>
-									{/snippet}
-								</ContextMenu.Trigger>
-								<ContextMenu.Content>
-									<!-- TODO: file previews -->
-									<ContextMenu.Item
-										class="flex gap-2"
-										disabled={is_dir || !ext || !previewable.has(ext)}
-									>
-										<Eye class="size-4" /> Preview
-									</ContextMenu.Item>
-									<!-- TODO: folder downloading -->
-									<ContextMenu.Item
-										class="flex gap-2"
-										disabled={is_dir}
+			<Resizable.Pane defaultSize={defaultLayout[0]} minSize={10}>
+				<header class="flex flex-row items-center gap-4">
+					<Sidebar.Trigger />
+					{#if ctx.wad}
+						<Breadcrumb.Root>
+							<Breadcrumb.List>
+								<Breadcrumb.Item>
+									<Breadcrumb.Link
+										class="cursor-pointer"
 										onclick={() => {
-											download(item);
+											ctx.path = [];
+										}}>/</Breadcrumb.Link
+									>
+								</Breadcrumb.Item>
+								{#each ctx.path as path, i}
+									<Breadcrumb.Separator />
+									<Breadcrumb.Item>
+										<Breadcrumb.Link
+											class="cursor-pointer"
+											onclick={() => {
+												ctx.path.splice(i + 1);
+											}}>{ctx.wad.get(path)?.name}</Breadcrumb.Link
+										>
+									</Breadcrumb.Item>
+								{/each}
+							</Breadcrumb.List>
+						</Breadcrumb.Root>
+					{/if}
+				</header>
+
+				{#if !browserOpen}
+					<div
+						class="flex flex-grow"
+						in:fade={{ delay: 100, duration: 100 }}
+						out:fade={{ duration: 100 }}
+					>
+						<DropZone class="m-5 flex-grow" {onFiles}>
+							<h2>No file open.</h2>
+							<p class="text-sm text-muted-foreground">Drag and drop a file or</p>
+							<Button>Upload<Upload /></Button>
+						</DropZone>
+					</div>
+				{:else}
+					<section class="mt-5 flex-grow">
+						<ul
+							in:fade={{ delay: 100, duration: 100 }}
+							out:fade={{ duration: 100 }}
+							class="grid grid-cols-[min-content,minmax(0,1fr)] items-center gap-x-2"
+						>
+							<li class="contents">
+								<TableEntry
+									directory
+									disabled={ctx.path.length == 0}
+									onclick={() => {
+										ctx.path.splice(-1);
+									}}
+									class="cursor-pointer"
+								>
+									<Undo2 class="size-4" />
+									...
+								</TableEntry>
+							</li>
+							{#each view as item}
+								{@const is_dir = item.is_dir()}
+								{@const ext = item.name.split('.').at(-1)}
+								{@const Icon =
+									(ext !== undefined && file_icons[ext]) || (is_dir ? Folder : FileIcon)}
+								<li class="contents">
+									<ContextMenu.Root
+										onOpenChange={(open) => {
+											if (!open) return;
+											ctx.selected = item.id;
 										}}
 									>
-										<Download class="size-4" /> Download
-									</ContextMenu.Item>
-									{#if ext == 'bin'}
-										<ContextMenu.Separator />
-										<ContextMenu.Item
-											class="flex gap-2"
-											onclick={() => {
-												const data = load_chunk(item.id);
-												if (!data) return;
-												try {
-													bin_ctx.bin = Bin.from_bytes(data);
-												} catch (e) {
-													console.error(e);
-													toast.error(`${e}`);
-												}
-												goto(base + '/bin');
-											}}
-										>
-											<Table2 class="size-4" /> Open in Bin Explorer
-										</ContextMenu.Item>
-									{/if}
-								</ContextMenu.Content>
-							</ContextMenu.Root>
-						</li>
-					{/each}
-				</ul>
-			</section>
-		</DropOverlay>
-	{/if}
+										<ContextMenu.Trigger>
+											{#snippet child({ props })}
+												<TableEntry
+													{...props}
+													selected={item.id === ctx.selected}
+													directory={is_dir}
+													onclick={() => {
+														ctx.selected = item.id;
+													}}
+													ondblclick={() => {
+														if (is_dir) {
+															ctx.path.push(item.id);
+														}
+													}}
+												>
+													<Icon class="size-4" />
+													<span class="truncate"> {item.name}</span>
+												</TableEntry>
+											{/snippet}
+										</ContextMenu.Trigger>
+										<ContextMenu.Content>
+											<!-- TODO: file previews -->
+											<ContextMenu.Item
+												class="flex gap-2"
+												disabled={is_dir || !ext || !previewable.has(ext)}
+												onclick={() => {
+													inspectorOpen = true;
+												}}
+											>
+												<Eye class="size-4" /> Preview
+											</ContextMenu.Item>
+											<!-- TODO: folder downloading -->
+											<ContextMenu.Item
+												class="flex gap-2"
+												disabled={is_dir}
+												onclick={() => {
+													download(item);
+												}}
+											>
+												<Download class="size-4" /> Download
+											</ContextMenu.Item>
+											{#if ext == 'bin'}
+												<ContextMenu.Separator />
+												<ContextMenu.Item
+													class="flex gap-2"
+													onclick={() => {
+														const data = load_chunk(item.id);
+														if (!data) return;
+														try {
+															bin_ctx.bin = Bin.from_bytes(data);
+														} catch (e) {
+															console.error(e);
+															toast.error(`${e}`);
+														}
+														goto(base + '/bin');
+													}}
+												>
+													<Table2 class="size-4" /> Open in Bin Explorer
+												</ContextMenu.Item>
+											{/if}
+										</ContextMenu.Content>
+									</ContextMenu.Root>
+								</li>
+							{/each}
+						</ul>
+					</section>
+				{/if}
+			</Resizable.Pane>
+			{#if inspectorOpen}
+				<Resizable.Handle withHandle />
+				<Resizable.Pane
+					class={cn('', isCollapsed && 'max-w-max')}
+					defaultSize={defaultLayout[1]}
+					minSize={5}
+					collapsible
+					collapsedSize={0}
+					{onResize}
+					{onCollapse}
+					{onExpand}
+				>
+					inspector
+				</Resizable.Pane>
+			{/if}
+		</Resizable.PaneGroup>
+	</DropOverlay>
 </main>
